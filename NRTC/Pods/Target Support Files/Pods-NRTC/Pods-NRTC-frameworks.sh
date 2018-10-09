619ec17df6151d5244d687e216edef502ddcfdfd
Usama Naseer <unaseer@netpace.com>
2018-10-09 17:39:37 +0500

    Login Validation

--- NRTC/Pods/Target Support Files/Pods-NRTC/Pods-NRTC-frameworks.sh
+++ NRTC/Pods/Target Support Files/Pods-NRTC/Pods-NRTC-frameworks.sh
@@ -143,10 +143,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ImageSlideshow/ImageSlideshow.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SideMenu/SideMenu.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ImageSlideshow/ImageSlideshow.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SideMenu/SideMenu.framework"
 fi
